19a65b9178c4cc38247f546161f1b368a29c80ba
NICOLA-200 <fatepepe66@gmail.com>
2023-09-04 11:36:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
