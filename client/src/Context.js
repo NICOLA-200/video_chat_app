089b12f7bd1423af64d956a111b049ca33dba01d
NICOLA-200 <fatepepe66@gmail.com>
2023-09-05 18:11:34 +0200

    ended the context

--- client/src/Context.js
+++ client/src/Context.js
@@ -45,4 +45,62 @@ const ContextProvider = ({ children }) => {
       socket.emit('answerCall', { signal: data, to: call.from });
     });
 
- 
+    peer.on('stream', (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    connectionRef.current = peer;
+  };
+
+  const callUser = (id) => {
+    const peer = new Peer({ initiator: true, trickle: false, stream });
+
+    peer.on('signal', (data) => {
+      socket.emit('callUser', { userToCall: id, signalData: data, from: me, name });
+    });
+
+    peer.on('stream', (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    socket.on('callAccepted', (signal) => {
+      setCallAccepted(true);
+
+      peer.signal(signal);
+    });
+
+    connectionRef.current = peer;
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+
+    connectionRef.current.destroy();
+
+    window.location.reload();
+  };
+
+  return (
+    <SocketContext.Provider value={{
+      call,
+      callAccepted,
+      myVideo,
+      userVideo,
+      stream,
+      name,
+      setName,
+      callEnded,
+      me,
+      callUser,
+      leaveCall,
+      answerCall,
+    }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export { ContextProvider, SocketContext };
