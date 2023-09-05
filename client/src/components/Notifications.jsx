28848b492240e576ef29b2d7623ec4880d11b08e
NICOLA-200 <fatepepe66@gmail.com>
2023-09-05 18:15:16 +0200

    started the notification

---
+++ client/src/components/Notifications.jsx
@@ -0,0 +1,9 @@
+import React, { useContext } from 'react';
+import { Button } from '@material-ui/core';
+
+import { SocketContext } from '../Context';
+
+const Notifications = () => {
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
+
+
