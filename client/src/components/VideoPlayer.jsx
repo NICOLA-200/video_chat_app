d9b21550b53557af61ca75e26f029092a501975a
NICOLA-200 <fatepepe66@gmail.com>
2023-09-05 18:17:45 +0200

    added the video

---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { Grid, Typography, Paper, makeStyles } from '@material-ui/core';
+
+import { SocketContext } from '../Context';
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: '550px',
+    [theme.breakpoints.down('xs')]: {
+      width: '300px',
+    },
+  },
+  gridContainer: {
+    justifyContent: 'center',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  paper: {
+    padding: '10px',
+    border: '2px solid black',
+    margin: '10px',
+  },
+}));
+
