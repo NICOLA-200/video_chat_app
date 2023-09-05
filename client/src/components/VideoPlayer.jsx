3e24e22862b6ef6dfa7a614489b2f9cc87be37b0
NICOLA-200 <fatepepe66@gmail.com>
2023-09-05 18:18:21 +0200

    ended the media

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -38,4 +38,16 @@ const VideoPlayer = () => {
           </Grid>
         </Paper>
       )}
-    
+      {callAccepted && !callEnded && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
+            <video playsInline ref={userVideo} autoPlay className={classes.video} />
+          </Grid>
+        </Paper>
+      )}
+    </Grid>
+  );
+};
+
+export default VideoPlayer;
