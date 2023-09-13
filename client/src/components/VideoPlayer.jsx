a82b9af83ba0f66f378dbf48e1146096294a22ef
NICOLA-200 <fatepepe66@gmail.com>
2023-09-13 10:35:12 +0200

    vidoePlayer started

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -22,32 +22,3 @@ const useStyles = makeStyles((theme) => ({
     margin: '10px',
   },
 }));
-
-
-const VideoPlayer = () => {
-  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
-  const classes = useStyles();
-
-  return (
-    <Grid container className={classes.gridContainer}>
-      {stream && (
-        <Paper className={classes.paper}>
-          <Grid item xs={12} md={6}>
-            <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
-            <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
-          </Grid>
-        </Paper>
-      )}
-      {callAccepted && !callEnded && (
-        <Paper className={classes.paper}>
-          <Grid item xs={12} md={6}>
-            <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
-            <video playsInline ref={userVideo} autoPlay className={classes.video} />
-          </Grid>
-        </Paper>
-      )}
-    </Grid>
-  );
-};
-
-export default VideoPlayer;
